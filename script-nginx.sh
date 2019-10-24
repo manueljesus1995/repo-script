c23b04bee4af5cec974376617c004383002826b8
Tu Nombre <you@example.com>
2019-10-24 12:10:00 +0200

    repo-script.04

--- script-nginx.sh
+++ script-nginx.sh
@@ -9,7 +9,7 @@ apt-get install nginx -y
 
 #Instalamos phppp-fpm para iteraccionar con el sistema gestor de base de datos
 
-apt-get install php-fpm php-mysql -y
+apt-get install php-fpm php-mysql
 
 #Configuramos php-fpm cambiando una linea de codifo por otra solo modificamos el ; que es liminado y el 1 por el 0
 
@@ -21,21 +21,23 @@ systemctl restart php7.2-fpm -y
 
 #Configuramos nginx para php
 
-cd /etc/nginx/sites-available/
+rm /etc/nginx/sites-available/default
 
-rm default
-
-cd /home/ubuntu
+#Descargamos el archivo de configuracion modificado de mi repositorio
 
 git clone https://github.com/manueljesus1995/archivos-conf.git
 
+#Entramos en el repositorio
+
 cd /archivos-conf
+
+#movemos el archivo situado en la primara ruta a la ruta de destino que es la segunda
 
 mv /home/ubuntu/archivos-conf/default /etc/nginx/sites-available/
 
 cd /var/www/html
 
-#Nos bajamos el repositorio
+#Nos bajamos el repositorio de la bse de datos
 
 git clone https://github.com/josejuansanchez/iaw-practica-lamp
 
@@ -45,8 +47,9 @@ chown www-data:www-data * -R
 
 #Accedemos al directorio y configuramos la base de datos para que busque en la maquina en la que tenemos mysql en lugar de buscar en localhost
 
-sed -i 's/localhost/54.147.58.211/' /var/www/html/iaw-practica-lamp/src/config.php
+sed -i 's/localhost/54.147.58.211/' /iaw-practica-lamp/src/config.php
 
 #Reiniciamos nginx
 
 systemctl restart nginx
+
