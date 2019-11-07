676b9cac456b56f09ab2deda0085fbf6e1c3dff4
Tu Nombre <you@example.com>
2019-11-07 12:30:43 +0100

    repo-script.15

--- nfsserver8.sh
+++ nfsserver8.sh
@@ -12,7 +12,7 @@ chown nobody:nogroup /var/www/html/wordpress/wp-content
 
 #Añadimos la siguiente línea al archivo exports
 
-echo "/var/www/html/wp-content      35.153.207.97(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/var/www/html/wordpress/wp-content      35.153.207.97(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 
 #Reiniciamos el servicio nfs
 
